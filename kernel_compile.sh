f6e46d9e802d10850f4a2eafad1a5239f98b36b1
ElectronSpark <electronsparkle@gmail.com>
2024-06-01 16:43:36 -0400

    add reference to Microsoft's mannual on how to compile WSL kernel.

--- kernel_compile.sh
+++ kernel_compile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Follow this: https://learn.microsoft.com/en-us/community/content/wsl-user-msft-kernel-v6
+
 git clone https://github.com/microsoft/WSL2-Linux-Kernel.git --depth=1 -b linux-msft-wsl-6.1.y
 sudo apt update -y
 sudo apt install build-essential flex bison libssl-dev libelf-dev bc python3 pahole
