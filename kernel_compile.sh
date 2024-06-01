af3773cd403ed7db84c2fac32eb90d97e52856a4
ElectronSpark <electronsparkle@gmail.com>
2024-06-01 15:39:39 -0400

    add host kernel compile script

--- kernel_compile.sh
+++ kernel_compile.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+git clone https://github.com/microsoft/WSL2-Linux-Kernel.git --depth=1 -b linux-msft-wsl-6.1.y
+sudo apt update -y
+sudo apt install build-essential flex bison libssl-dev libelf-dev bc python3 pahole
+cd WSL2-Linux-Kernel
+make -j$(nproc) KCONFIG_CONFIG=../config/wsl/.config
+# sudo make modules_install headers_install
